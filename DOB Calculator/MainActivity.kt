973e38f7469fea2b190250aff0b3ea7cb1dc1e18
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-09-15 17:32:04 +0530

    added explanation comments

--- DOB Calculator/MainActivity.kt
+++ DOB Calculator/MainActivity.kt
@@ -38,19 +38,30 @@ class MainActivity : AppCompatActivity() {
 //                Toast.makeText(this, "date selected", Toast.LENGTH_LONG).show()
 
                 val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/${selectedYear}"
+//                val selectedDate = "${selectedMonth+1}/$selectedDayOfMonth/${selectedYear}"
                 tvSelectedDate?.text = selectedDate
 
                 //Creating object of simple date format
+                //SimpleDateFormat: a concrete class for formatting and parsing dates in a locale-sensitive manner.
+                //A Locale object represents a specific geographical, political, or cultural region.
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+//                val sdf = SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH)
+
+
                 //Creating actual date format from sdf
+                //Parses text from a string to produce a Date object.
                 val theDate = sdf.parse(selectedDate)
 
                 theDate?.let {
+                    //Date.getTime() -> time: converts given date into minutes from jan 1, 1970 00:00:00 GMT
                     val selectedDateInMinutes = theDate.time / 60000
 
+                    //sdf.format Formats the given Date into a date/time string and appends the result to the given StringBuffer.
+                    //System.currentTimeMillis() converts system time into minutes from jan 1, 1970 00:00:00 GMT
                     val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
                     currentDate?.let {
-                        val currentDateInMinutes = currentDate.time/ 60000
+                        val currentDateInMinutes = currentDate.time / 60000
                         val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
                         tvAgeMinutes?.text = differenceInMinutes.toString()
                     }
