36c4480bbcc710d08ef748626220fc49776170ed
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-10-11 22:46:32 +0530

    Initial Commit Myfragment

---
+++ Myfragment/app/src/main/java/com/example/myfragment/FirstFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myfragment.databinding.ActivityMainBinding
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+//    private val _binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+    /*
+    * Alternate way of passing argument in fragment constructor
+     */
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_first, container, false)
+//    }
+
+
+
+}
