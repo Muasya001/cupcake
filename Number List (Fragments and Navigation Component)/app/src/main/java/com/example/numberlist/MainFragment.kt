63ca49712ac973fee06341dc5bdcd32f7390ed90
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-10-31 16:58:02 +0530

    fragmentmainbinding

--- Number List (Fragments and Navigation Component)/app/src/main/java/com/example/numberlist/MainFragment.kt
+++ Number List (Fragments and Navigation Component)/app/src/main/java/com/example/numberlist/MainFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.numberlist.databinding.FragmentMainBinding
 import com.example.practice.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
