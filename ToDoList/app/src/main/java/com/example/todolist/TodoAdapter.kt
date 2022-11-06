6a6530121b271734cc22b52ed0cea0a535e160e9
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-11-06 12:02:06 +0530

    modified delete listener

--- ToDoList/app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ ToDoList/app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -5,29 +5,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.databinding.ItemViewBinding
 
-class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ItemViewHolder>() {
-
-    inner class ItemViewHolder(private val binding: ItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
-        val tvItem = binding.tvItem
-        val btnDelete = binding.btnDelete
-    }
+class TodoAdapter : RecyclerView.Adapter<ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ItemViewHolder(binding)
+        val itemViewHolder = ItemViewHolder(binding)
+        itemViewHolder.btnDelete.setOnClickListener {
+            todoList.removeAt(itemViewHolder.adapterPosition)
+            notifyItemRemoved(itemViewHolder.adapterPosition)
+        }
+        return itemViewHolder
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.tvItem.text = todoList[position].task
-
-        holder.btnDelete.setOnClickListener {
-            todoList.removeAt(holder.adapterPosition)
-            notifyItemRemoved(holder.adapterPosition)
-        }
     }
 
     override fun getItemCount(): Int {
         return todoList.size
     }
 
+}
+class ItemViewHolder(private val binding: ItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
+    val tvItem = binding.tvItem
+    val btnDelete = binding.btnDelete
 }
